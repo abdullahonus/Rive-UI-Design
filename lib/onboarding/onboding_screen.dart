b12bcf30c70b9b9f66cadbba4f39f892f2d71eb4
ao <abdullah.onus0@gmail.com>
2022-12-15 12:04:07 +0300

    Countrol4

---
+++ lib/onboarding/onboding_screen.dart
@@ -0,0 +1,127 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+import 'components/animated_btn.dart';
+import 'components/sign_in_dialog.dart';
+
+class OnbodingScreen extends StatefulWidget {
+  const OnbodingScreen({super.key});
+
+  @override
+  State<OnbodingScreen> createState() => _OnbodingScreenState();
+}
+
+class _OnbodingScreenState extends State<OnbodingScreen> {
+  late RiveAnimationController _btnAnimationController;
+
+  bool isShowSignInDialog = false;
+
+  @override
+  void initState() {
+    _btnAnimationController = OneShotAnimation(
+      "active",
+      autoplay: false,
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black54,
+      body: Stack(
+        children: [
+          Positioned(
+            top: 1,
+            width: MediaQuery.of(context).size.width * 1.7,
+            child: Image.asset(
+              "assets/Backgrounds/Spline.png",
+              height: 500,
+            ),
+          ),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
+              child: const SizedBox(),
+            ),
+          ),
+          const RiveAnimation.asset(
+            "assets/RiveAssets/shapes.riv",
+          ),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+              child: const SizedBox(),
+            ),
+          ),
+          AnimatedPositioned(
+            top: isShowSignInDialog ? -100 : 0,
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            duration: const Duration(milliseconds: 260),
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Spacer(),
+                    SizedBox(
+                      width: 500,
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            "assets/c4.png",
+                            fit: BoxFit.contain,
+                          ),
+                          const SizedBox(height: 150),
+                          const Text(
+                            "Don’t skip design. Learn design and code, by building real apps with Flutter. Complete courses about the best tools.",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Spacer(flex: 4),
+                    AnimatedBtn(
+                      btnAnimationController: _btnAnimationController,
+                      press: () {
+                        _btnAnimationController.isActive = true;
+
+                        Future.delayed(
+                          const Duration(seconds: 1),
+                          () {
+                            setState(() {
+                              isShowSignInDialog = true;
+                            });
+                            showCustomDialog(
+                              context,
+                              onValue: (_) {
+                                setState(() {
+                                  isShowSignInDialog = false;
+                                });
+                              },
+                            );
+                          },
+                        );
+                      },
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 24),
+                      child: Text(
+                        "Enjoy Coding W/Flutter",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
